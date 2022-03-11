5ea7db715b483bd3b868a1ed1b4f72426afc532e
biocom <31534717+biocom@users.noreply.github.com>
2022-03-11 17:04:44 +0000

    Add dot

--- pages/index.tsx
+++ pages/index.tsx
@@ -55,7 +55,7 @@ export default function Home({ metricsData, siteConfigData }) {
         <div>
           <SectionImage margin={'0 auto -18rem'} height={'68rem'}><img loading="lazy" src="/images/cowBelt.jpg" alt="A fast growing protocol" /></SectionImage>
           <h2>A fast-growing trading protocol</h2>
-          <SubTitle align="center">Trade on CoW Protocol for <br /> better prices, gas cost savings and extra secure MEV protection <ExternalLink href="https://dune.xyz/gnosis.protocol/Gnosis-Protocol-V2">View analytics</ExternalLink></SubTitle>
+          <SubTitle align="center">Trade on CoW Protocol for <br /> better prices, gas cost savings and extra secure MEV protection. <ExternalLink href="https://dune.xyz/gnosis.protocol/Gnosis-Protocol-V2">View analytics</ExternalLink></SubTitle>
           <Metrics>
             {metricsData.map(({ label, value }, i) =>
               <div key={i}>
