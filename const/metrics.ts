497f5a05056f7ca7f34f01e436662df1f22f821b
Anxo Rodriguez <anxolin@gmail.com>
2022-04-02 20:44:00 +0100

    Update metrics

--- const/metrics.ts
+++ const/metrics.ts
@@ -1,11 +1,12 @@
 const metrics = [
   // https://dune.xyz/gnosis.protocol/Gnosis-Protocol-V2
-  {label: "Total Volume", value: "$7B+"},
-  {label: "All Time Trades", value: "220K+"},
+  {label: "Total Volume", value: "$8B+"},
+  
+  {label: "All Time Trades", value: "249K+"},
 
   // https://dune.xyz/gnosis.protocol/GPv2-Trader-Surplus
   //  Resonable + Unusual
-  {label: "Surplus generated for users", value: "$37M+"}
+  {label: "Surplus generated for users", value: "$43M+"}
 ]
 
 export default metrics
