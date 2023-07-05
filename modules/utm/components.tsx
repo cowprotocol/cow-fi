eeb2cbdf8e4f25b8c47cad8ab2522ad7a189e476
Anxo Rodriguez <anxolin@gmail.com>
2023-07-05 20:54:29 +0100

    Add support for default UTM if not present

--- modules/utm/components.tsx
+++ modules/utm/components.tsx
@@ -1,19 +1,24 @@
 import { useMemo } from 'react'
 import Link, { LinkProps } from 'next/link'
 import { useRouter } from 'next/router'
-import { useUtm } from 'modules/utm'
+import { UtmParams, useUtm } from 'modules/utm'
 import { addUtmToUrl, hasUtmCodes } from 'modules/utm/utils'
 
-export function LinkWithUtm(p: React.PropsWithChildren<LinkProps>): JSX.Element {
-  const { href, as, children, ...props } = p
+interface LinkWithUtmProps extends React.PropsWithChildren<LinkProps> {
+  defaultUtm?: UtmParams
+}
+
+export function LinkWithUtm(p: LinkWithUtmProps): JSX.Element {
+  const { href, as, children, defaultUtm, ...props } = p
   const utm = useUtm()
 
   const newHref = useMemo(() => {
-    if (hasUtmCodes(utm) && typeof href === 'string') {
-      return addUtmToUrl(href, utm)
+    const utmAux = utm || defaultUtm
+    if (hasUtmCodes(utmAux) && typeof href === 'string') {
+      return addUtmToUrl(href, utmAux)
     }
     return href
-  }, [utm, href])
+  }, [utm, defaultUtm, href])
 
   return (
     <Link href={newHref} as={as} {...props}>
