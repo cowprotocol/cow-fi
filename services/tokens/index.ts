a6054e9ee4d4e59afa00c61ad3006d66bad696a1
fairlight <31534717+fairlighteth@users.noreply.github.com>
2023-07-07 15:08:28 +0100

    feat: iterate on meta description templates (#117)
    * feat: iterate on tempaltes
    
    * feat: iterate on template
    
    * feat: iterate on template
    
    * feat: iterate on template

--- services/tokens/index.ts
+++ services/tokens/index.ts
@@ -2,6 +2,7 @@ import fs from 'fs'
 import path from 'path'
 import { PlatformData, Platforms, TokenDetails, TokenInfo } from 'types'
 import { backOff } from 'exponential-backoff'
+import { META_DESCRIPTION_TEMPLATES } from '@/const/meta'
 
 const NETWORKS = ['ethereum', 'xdai']
 const COW_TOKEN_ID = 'cow-protocol'
@@ -125,29 +126,14 @@ function _getTokenMetaDescription(token: TokenDetails): string {
   }
 
   const { name, symbol, priceUsd, volume, change24h, marketCap } = token;
-
   const change24hTrimmed = parseFloat(change24h).toFixed(2);
   const isIncrease = parseFloat(change24h) >= 0;
   const changeDirection = isIncrease ? 'increase ▲' : 'decrease ▼';
-  
-  const templates = [
-    `Moo-ve over to ${name} (${symbol})! Grazing at $${priceUsd} with ${changeDirection} of ${change24hTrimmed}% in 24h. Trading volume: $${volume}. Their market cap: $${marketCap}. Learn about ${symbol}'s pasture.`,
-    `The grass is greener with ${name} (${symbol})! At $${priceUsd}, with ${changeDirection} of ${change24hTrimmed}% in 24h. Trading volume: $${volume}. Their market cap: $${marketCap}. Discover more about ${symbol}.`,
-    `Interested in ${name} (${symbol})? Priced at $${priceUsd}, with ${changeDirection} of ${change24hTrimmed}% in 24h. Trading volume: $${volume}. Their market cap: $${marketCap}. Learn more about ${symbol}!`,
-    `Graze on this: ${name} (${symbol}) at $${priceUsd}, with ${changeDirection} of ${change24hTrimmed}% in 24h. Trading volume: $${volume}. They boast a market cap of $${marketCap}. Learn about ${symbol}.`,
-    `Ever heard of ${name} (${symbol})? At $${priceUsd}, they've marked a ${changeDirection} of ${change24hTrimmed}% in 24h. Trading volume: $${volume}. Their market cap: $${marketCap}. Get to know them.`,
-    `Check out ${name} (${symbol})! Grazing at $${priceUsd}, with ${changeDirection} of ${change24hTrimmed}% in 24h. Trading volume: $${volume}. Their market cap: $${marketCap}. Discover ${symbol}'s secrets.`,
-    `Latest on ${name} (${symbol}): priced at $${priceUsd}. Experienced a ${changeDirection} of ${change24hTrimmed}% in 24h. Trading volume: $${volume}. Their market cap: $${marketCap}. Learn more.`,
-  ];  
-  
-  const randomIndex = Math.floor(Math.random() * templates.length);
-  
-  return templates[randomIndex];
+  const randomIndex = Math.floor(Math.random() * META_DESCRIPTION_TEMPLATES.length);
+  const chosenTemplate = META_DESCRIPTION_TEMPLATES[randomIndex];
+
+  return chosenTemplate({ name, symbol, changeDirection, priceUsd, change24hTrimmed, volume, marketCap });
 }
-
-
-
-
 
 function _toTokenDetails(tokenRaw: any, description: string): TokenDetails {
   // Add platform information
