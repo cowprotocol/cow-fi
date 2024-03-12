63117c619d3655734c40db13fb7c62856a6a1d49
Alfetopito <alfetopito@users.noreply.github.com>
2024-03-12 12:59:12 +0000

    feat: add cowswag

---
+++ pages/cowswag.tsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import { Widget } from '@typeform/embed-react'
+import { GetStaticProps } from 'next'
+import { CONFIG } from '@/const/meta'
+import Layout from '@/components/Layout'
+import Head from 'next/head'
+import { Section, SectionContent, SectionH1, SectionImage } from '@/components/Home/index.styles'
+import { Color, TextColor } from '@/styles/variables'
+
+const IMAGE_PATH = '/images/'
+const MAX_WIDTH_CONTENT = 126
+const TYPEFORM_ID = 'U8gDXwrd' // Replace with your Typeform embed URL
+
+export default function CoWSwag() {
+  const pageTitle = `CoW Swag`
+  const pageDescription = 'The official CoW Swap Swag store.'
+  // TODO: cow swag img
+  const pageImage = `${CONFIG.url.root}${IMAGE_PATH}og-social-image-cowamm.png`
+
+  return (
+    <Layout>
+      <Head>
+        <title>{pageTitle}</title>
+        <meta key="description" name="description" content={pageDescription} />
+        <meta key="ogTitle" property="og:title" content={pageTitle} />
+        <meta key="ogDescription" property="og:description" content={pageDescription} />
+        <meta key="ogImage" property="og:image" content={pageImage} />
+        <meta key="twitterTitle" name="twitter:title" content={pageTitle} />
+        <meta key="twitterDescription" name="twitter:description" content={pageDescription} />
+        <meta key="twitterImage" name="twitter:image" content={pageImage} />
+      </Head>
+
+      <Section
+        gapMobile={4}
+        fullWidth
+        firstSection
+        padding={'8rem 8rem 4rem'}
+        paddingMobile={'0 2.4rem 4rem'}
+        flow="column"
+      >
+        {/*TODO: cowswag animated logo*/}
+        <SectionImage width={'460px'} widthMobile="90%" margin="0 auto" className="zoomSlideIn">
+          <img src={`${IMAGE_PATH}cowamm-header-logo-animated.gif`} alt="CoW Swag" width="100%" />
+        </SectionImage>
+        <SectionContent flow="column" maxWidth={MAX_WIDTH_CONTENT}>
+          <div>
+            <SectionH1
+              color={Color.cowammWhite}
+              fontSize={6.6}
+              fontSizeMobile={3.6}
+              fontWeight={500}
+              maxWidth={100}
+              margin={'0 auto 4rem'}
+            >
+              CoW Swag, the official CoW Swap Swag store. By
+              <TextColor italic color={'cowammYellow'}>
+                CoW DAO
+              </TextColor>
+            </SectionH1>
+          </div>
+        </SectionContent>
+      </Section>
+
+      <Section fullWidth padding={'0'}>
+        <SectionContent maxWidth={MAX_WIDTH_CONTENT}>
+          <div>
+            <SectionH1
+              color={Color.cowammWhite}
+              fontSize={6.6}
+              fontSizeMobile={3.6}
+              fontWeight={500}
+              maxWidth={100}
+              margin={'0 auto 4rem'}
+            >
+              Order your Swag now!
+            </SectionH1>
+            <Widget id={TYPEFORM_ID} style={{ width: '100%', height: '600px' }} className="my-form" />
+          </div>
+        </SectionContent>
+      </Section>
+    </Layout>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const siteConfigData = CONFIG
+
+  return {
+    props: { siteConfigData },
+  }
+}
