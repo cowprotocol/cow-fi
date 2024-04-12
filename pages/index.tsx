383a7bf88ce53e1b078a1790d418643d92d71583
Anxo Rodriguez <anxolin@gmail.com>
2024-04-12 13:42:44 +0200

    Add default value for USD volume in case the query fails

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,13 @@ import Layout from '@/components/Layout'
 import { CowSdk } from '@cowprotocol/cow-sdk'
 import { getCowStats } from 'services/cow'
 import Home, { HomeProps } from '@/components/Home'
+
 const cowSdk = new CowSdk(1)
 const numberFormatter = Intl.NumberFormat('en', { notation: 'compact' })
 const DATA_CACHE_TIME_SECONDS = 5 * 60 // Cache 5min
+
+// Defaults are only meant to be used when the API fails
+const DEFAULT_USD_VOLUME = '2548024217' // https://dune.com/cowprotocol/cowswap-high-level-metrics-dashboard?Aggregate+by_e759c2=Week
 
 export default function HomePage({ metricsData, siteConfigData }: HomeProps) {
   return (
@@ -28,16 +32,14 @@ export default function HomePage({ metricsData, siteConfigData }: HomeProps) {
 
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
   const siteConfigData = CONFIG
-  let volumeUsd = 0
-  let volumeEth = 0
+  let volumeUsd = DEFAULT_USD_VOLUME
 
   // Don't fail when couldn't get Subgraph data
   try {
     const data = await cowSdk.cowSubgraphApi.getTotals()
     volumeUsd = data.volumeUsd
-    volumeEth = data.volumeEth
   } catch (e) {
-    console.error(e)
+    console.error('Error getting totals from Dune', e)
   }
   const { surplus, totalTrades, lastModified } = await getCowStats()
 
@@ -48,7 +50,6 @@ export const getStaticProps: GetStaticProps<HomeProps> = async () => {
     props: {
       metricsData: {
         totalVolume: numberFormatter.format(+volumeUsd) + '+',
-        totalVolumeETH: numberFormatter.format(+volumeEth) + '+',
 
         tradesCount: numberFormatter.format(totalTrades) + '+',
         tradesCountLastModified: lastModifiedFormatted,
